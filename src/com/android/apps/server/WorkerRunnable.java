02d66ba677d397d848ddff06c8a36f10864917a1
nsiddharth <n.siddharth@gail.com>
2014-08-24 22:26:56 -0700

    modified server to do magic which is not happening

--- src/com/android/apps/server/WorkerRunnable.java
+++ src/com/android/apps/server/WorkerRunnable.java
@@ -3,8 +3,8 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.Socket;
+import java.util.ArrayList;
 
 /**
 
@@ -15,46 +15,59 @@ public class WorkerRunnable implements Runnable{
 	
     protected Socket clientSocket = null;
     protected String serverText   = null;
+    protected ArrayList<Socket> connList = null;
 
-    public WorkerRunnable(Socket clientSocket, String serverText) {
+    public WorkerRunnable(Socket clientSocket, String serverText, MultiThreadedServer parent) {
         this.clientSocket = clientSocket;
         this.serverText   = serverText;
+       this.connList = parent.getClients();
     }
 
     public void run() {
         try {
-            InputStream input  = clientSocket.getInputStream();
+        	
+        	InputStream input  = clientSocket.getInputStream();
             DataInputStream dis = new DataInputStream(input);
-            OutputStream output = clientSocket.getOutputStream();
-            DataOutputStream dos = new DataOutputStream(output);
-            long time = System.currentTimeMillis();
-                 
-            int length = dis.readInt();
-            byte[] bArray = new byte[length];
-			dis.readFully(bArray, 0, length);
-			//fromClientSocket.getInputStream().read(bArray);
-			//ByteArrayInputStream bis = new ByteArrayInputStream(bArray);
-			//fromClient = new ObjectInputStream(bis);
-			
-			//PaintObject po = (PaintObject) SerializationUtils.deserialize(bArray);
-			
-			
-			//System.out.println("Received Object : "+ po.kuchBhi);
-			
-			dos.writeInt(length);
-			dos.write(bArray);
-            
-            
-          
+           // OutputStream output = clientSocket.getOutputStream();
+           //DataOutputStream dos = new DataOutputStream(output);
            
-            output.close();
-            input.close();
-            dis.close();
-            dos.close();
+            DataOutputStream dos = null;
+            long time = System.currentTimeMillis();
+            
+            if(dis.available()>0){
+            	int length = dis.readInt();
+
+            	byte[] bArray = new byte[length];
+            	dis.readFully(bArray, 0, length);
+            	//fromClientSocket.getInputStream().read(bArray);
+            	//ByteArrayInputStream bis = new ByteArrayInputStream(bArray);
+            	//fromClient = new ObjectInputStream(bis);
+
+            	//PaintObject po = (PaintObject) SerializationUtils.deserialize(bArray);
+
+
+            	System.out.println("Connection size Object : "+ connList.size());
+            	for(Socket clientSocket : connList)
+            	{
+            		dos = new DataOutputStream(clientSocket.getOutputStream());
+            		dos.writeInt(length);
+            		dos.write(bArray);
+            	}
+            }
+            if(input!=null)
+            	input.close();
+            if(dis!=null)
+            	dis.close();
+            if(dos!=null)
+            	dos.close();
             System.out.println("Request processed: " + time);
+          
+        	 
+           
         } catch (IOException e) {
             //report exception somewhere.
             e.printStackTrace();
         }
+        
     }
 }
