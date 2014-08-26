a8b0b5b497b7534b6802316d836ac56e0943becd
nsiddharth <n.siddharth@gail.com>
2014-08-25 22:07:50 -0700

    commit server changes

--- src/com/android/apps/server/MultiThreadedServer.java
+++ src/com/android/apps/server/MultiThreadedServer.java
@@ -1,97 +1,155 @@
 package com.android.apps.server;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 
 public class MultiThreadedServer implements Runnable{
 
-    protected int          serverPort   = 8080;
-    protected ServerSocket serverSocket = null;
-    protected boolean      isStopped    = false;
-    protected Thread       runningThread= null;
-    protected ArrayList<Socket> connList = new ArrayList<Socket>() ;
+	protected int          serverPort   = 9002;
+	protected ServerSocket serverSocket = null;
+	protected boolean      isStopped    = false;
+	protected static ArrayList<Socket> connList = new ArrayList<Socket>() ;
 
-    public MultiThreadedServer(int port){
-        this.serverPort = port;
-    }
-    
-    public ArrayList<Socket> getClients(){
-    	return connList;
-    }
-    
-    
-    public synchronized void removeSocket(Socket socket){
-    	if(connList.contains(socket)){
-    		connList.remove(socket);
-    	}
-    }
+	public MultiThreadedServer(int port){
+		this.serverPort = port;
+	}
 
-    public void run(){
-        synchronized(this){
-            this.runningThread = Thread.currentThread();
-        }
-        openServerSocket();
-        System.out.println("Started na");
-        while(! isStopped()){
-            Socket clientSocket = null;
-            try {
-          
-            	clientSocket = this.serverSocket.accept();
-                connList.add(clientSocket);
-         
-            } catch (IOException e) {
-                if(isStopped()) {
-                    System.out.println("Server Stopped.") ;
-                    return;
-                }
-                throw new RuntimeException(
-                    "Error accepting client connection", e);
-            }
-            new Thread(
-                new WorkerRunnable(
-                    clientSocket, "Multithreaded Server", this)
-            ).start();
-        }
-        System.out.println("Server Stopped.") ;
-    }
+	public ArrayList<Socket> getClients(){
+		return connList;
+	}
+
+	public synchronized void removeSocket(Socket socket){
+		if(connList.contains(socket)){
+			connList.remove(socket);
+		}
+	}
+
+	public void run(){
+		openServerSocket();
+		System.out.println("Started na");
+		while(! isStopped()){
+			Socket clientSocket = null;
+			try {
+
+				clientSocket = this.serverSocket.accept();
+				connList.add(clientSocket);
+
+			} catch (IOException e) {
+				if(isStopped()) {
+					System.out.println("Server Stopped.") ;
+					return;
+				}
+				throw new RuntimeException(
+						"Error accepting client connection", e);
+			}
+			new Thread(
+					new WorkerRunnable(
+							clientSocket, "Multithreaded Server")
+					).start();
+		}
+		System.out.println("Server Stopped.") ;
+	}
 
 
-    private synchronized boolean isStopped() {
-        return this.isStopped;
-    }
+	private synchronized boolean isStopped() {
+		return this.isStopped;
+	}
 
-    public synchronized void stop(){
-        this.isStopped = true;
-        try {
-            this.serverSocket.close();
-        } catch (IOException e) {
-            throw new RuntimeException("Error closing server", e);
-        }
-    }
+	public synchronized void stop(){
+		this.isStopped = true;
+		try {
+			this.serverSocket.close();
+		} catch (IOException e) {
+			throw new RuntimeException("Error closing server", e);
+		}
+	}
 
-    private void openServerSocket() {
-        try {
-            this.serverSocket = new ServerSocket(this.serverPort);
-        } catch (IOException e) {
-            throw new RuntimeException("Cannot open port", e);
-        }
-    }
-    
-    public static void main(String[] args)
-    {
-    	MultiThreadedServer server = new MultiThreadedServer(9002);
-    	new Thread(server).start();
+	private void openServerSocket() {
+		try {
+			this.serverSocket = new ServerSocket(this.serverPort);
+		} catch (IOException e) {
+			throw new RuntimeException("Cannot open port", e);
+		}
+	}
 
-    	/*try {
-    	   // Thread.sleep(20 * 1000);
-    	} catch (InterruptedException e) {
-    	    e.printStackTrace();
-    	}*/
-    	//System.out.println("Stopping Server");
-    	//server.stop();
-    	
-    }
+	public static void main(String[] args)
+	{
+		MultiThreadedServer server = new MultiThreadedServer(9002);
+		server.run();
+
+	}
+
+
+
+	private class WorkerRunnable implements Runnable{
+
+		protected Socket clientSocket = null;
+		protected String clientName   = null;
+		MultiThreadedServer parentServer = null;
+	
+		public WorkerRunnable(Socket clientSocket, String clientName) {
+			this.clientSocket = clientSocket;
+			this.clientName   = clientName;
+
+		}
+
+		@Override
+		public void run() {
+
+
+			InputStream input  = null;
+			DataInputStream dis = null;
+			
+			DataOutputStream dos = null;
+
+			try {
+				input = clientSocket.getInputStream();
+				dis = new DataInputStream(input);
+				while(true){
+					if(dis.available() > 0){
+						int length = dis.readInt();
+						if(length >0){
+							byte[] bArray = new byte[length];
+							dis.readFully(bArray, 0, length);
+							System.out.println("Connection size Object : "+ connList.size());
+							for(Socket clientSocket : connList)
+							{
+								dos = new DataOutputStream(clientSocket.getOutputStream());
+								dos.writeInt(length);
+								dos.write(bArray);
+							}
+						}else{
+							System.out.println("listening...");
+						}
+					}else{
+						//System.out.println("listening...");
+					}
+				}
+			} catch (IOException e) {
+				//report exception somewhere.
+				e.printStackTrace();
+			}finally{
+				try{
+				if(input!=null)
+					input.close();
+				if(dis!=null)
+					dis.close();
+				if(dos!=null)
+					dos.close();
+				}catch(IOException ex){
+					System.out.println("Exception in close");
+					ex.printStackTrace();
+				}
+				
+			}
+
+		}
+
+	}
 
 }
